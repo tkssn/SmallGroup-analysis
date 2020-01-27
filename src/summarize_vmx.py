648bf5230bba83d54899a6f6bd36725a8f0942b8
Takeshi <>
2020-01-28 02:16:09 +0900

    v1.3.0 - vmxのvNIC検索をネストの辞書形式に変更、JSONの出力に対応

--- src/summarize_vmx.py
+++ src/summarize_vmx.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 def SearchInVmxText(filepath,keyword):
     with open(filepath,'r') as fp:
@@ -6,6 +7,13 @@ def SearchInVmxText(filepath,keyword):
             matchword = re.match(keyword,line)
             if matchword:
                 return matchword.group(1)
+
+def SearchInVmxTwoElements(filepath,keyword):
+    with open(filepath,'r') as fp:
+        for line in fp:
+            list = re.findall(keyword,line)
+            if list:
+                return list
 
 def SearchInVmx_AllDeviceNumber(filepath,keyword):
     with open(filepath,'r') as fp:
@@ -15,6 +23,10 @@ def SearchInVmx_AllDeviceNumber(filepath,keyword):
             if result:
                 list.append(result[0])
         return list
+
+def Vmx_displayName(filepath):
+    keyword = r'displayName = "([a-zA-Z0-9]*)"'
+    return SearchInVmxText(filepath,keyword)
 
 def Vmx_virtualHWversion(filepath):
     keyword = r'virtualHW.version = "([0-9]*)"'
@@ -28,13 +40,20 @@ def Vmx_vMem(filepath):
     keyword = r'memSize = "([0-9]*)"'
     return SearchInVmxText(filepath,keyword)
 
-def Vmx_ethernet(filepath):
-    keyword = r'ethernet([0-9]).present = "TRUE"'
-    list = SearchInVmx_AllDeviceNumber(filepath,keyword)
+def Vmx_eth(filepath):
+    eth_number = r'(ethernet[0-9]*).present = "TRUE"'
+    eth_list = SearchInVmx_AllDeviceNumber(filepath,eth_number)
 
-    for item in list:
-        tmp = r'ethernet' + item + r'.virtualDev = "([a-zA-Z0-9]*)"'
-        print('eth' + item + ' : ' + SearchInVmxText(filepath,tmp))
+    for item in eth_list:
+        element0 = item + r'.(virtualDev) = "(.*)"'
+        element1 = item + r'.(networkName) = "(.*)"'
+        list1 = SearchInVmxTwoElements(filepath,element0)
+        list2 = SearchInVmxTwoElements(filepath,element1)
+        dict1 = dict(list1)
+        dict2 = dict(list2)
+        dict1.update(dict2)
+        dict3 = {item:dict1}
+        print(dict3)
     
 """
 def Vmx_scsi(filepath):
@@ -43,17 +62,24 @@ def Vmx_scsi(filepath):
 """
 
 def main():
-    tmp_filepath = '/Volumes/Macintosh HDD/code/sano/testVM.vmx'
+    tmp_filepath = '/Volumes/Macintosh HDD/code/SmallGroup-analysis/vmx/testVM.vmx'
 
     a = Vmx_virtualHWversion(tmp_filepath)
     b = Vmx_vCPU(tmp_filepath)
     c = Vmx_vMem(tmp_filepath)
-    Vmx_ethernet(tmp_filepath)
+    Vmx_eth(tmp_filepath)
+    d = Vmx_displayName(tmp_filepath)
 #    e = Vmx_scsi(tmp_filepath)
 
+    print("VM name : " +d)
     print("HW version : " + a)
     print("vCPU : " + b)
     print("vMem : " + c)
  #   print("scsi : " + e)
+
+    dict = {"VM name": d,"HW version": a,"vCPU": b,"vMem": c}
+    json_file = open('../json/test.json', 'w')
+    json.dump(dict, json_file)
+
 
 main()
