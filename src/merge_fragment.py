0d163c07192099f549077763d23230ae884ce325
Takeshi <>
2020-02-01 19:46:40 +0900

    v1.4.0 - tgzの解凍、vm-supportにおけるgzの解凍とFRAGファイルのマージに対応

---
+++ src/merge_fragment.py
@@ -0,0 +1,35 @@
+import os
+import re
+
+def MergeFileFragments(dirpath, suffix=r"[.]FRAG-[0-9]{5}", debug=False):
+    if not os.path.exists(dirpath):
+        return False
+    #
+    repattern = re.compile(r"^(.*)%s$" % suffix)
+    targets = {}
+    for filename in os.listdir(dirpath):
+        match = repattern.match(filename)
+        if match:
+            orgfile = match.groups()[0]
+            if not orgfile in targets:
+                targets[orgfile] = []
+            targets[orgfile].append(filename)
+    #
+    for orgfile in targets.keys():
+        targets[orgfile].sort()
+        with open(os.path.join(dirpath, orgfile), 'wb') as fporg:
+            for fragment in targets[orgfile]:
+                filepath = os.path.join(dirpath, fragment)
+                with open(filepath, 'rb') as fpfrag:
+                    fporg.write(fpfrag.read())
+                os.remove(filepath)
+    return True
+
+
+def main():
+    tmp_path = '/Volumes/Macintosh HDD/code/SmallGroup-analysis/LogBundle/esx-target.hitachi.local-2020-01-08--02.47/commands'
+
+    MergeFileFragments(tmp_path)
+
+
+main()
