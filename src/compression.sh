ac8b5544ae98ae55f1318695f22a21bf07584313
Takeshi <>
2020-02-05 23:13:31 +0900

    v1.6.1.1 - 圧縮機能のshを削除

--- src/compression.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-target_dir="/Volumes/Macintosh HDD/code/SmallGroup-analysis/LogBundle/testfolder"
-save_dir="/Volumes/Macintosh HDD/code/SmallGroup-analysis/LogBundle"
-date=`date "+%Y%m%d_%H%M%S"`
-save_file="/AutoCompress_$date.tar.gz"
-
-tar  -czf "$save_dir$save_file" -C "$(dirname "$target_dir")" "$(basename "$target_dir")"
